82cb7ff564a71083be48f8e8ec8b91251de49c71
Heubart <137958688+Heubart@users.noreply.github.com>
2023-06-29 02:00:09 +0600

    Update Class_Performance.m

--- Class_Performance.m
+++ Class_Performance.m
@@ -9,13 +9,13 @@ xlabel('Time');
 ylabel('Amplitude');
 title('Sawtooth Wave');
 
-subplot(2,1,2); %for 2nd row, 1st col, 2nd position
-T = 15*(1/60); %15 peeriod in 1 second
-fs = 1500; %fundamental freq
-t = 0:1/fs:T-1/fs; %sampling rate 0.5khz of 60hz fundamental freq/
-D=0.5; %duty cycle 50%
-y = sawtooth(2*pi*60*t,D); %function to draw tringular with duty cycle
-plot(t,y,'g'); %plotiing function
-xlabel('Time'); %show x-axes title
+subplot(2,1,2);  % for 2nd row, 1st col, 2nd position
+T = 15*(1/60);   % 15 peeriod in 1 second
+fs = 1500;       % fundamental freq
+t = 0:1/fs:T-1/fs; % sampling rate 0.5khz of 60hz fundamental freq/
+D=0.5;             % duty cycle 50%
+y = sawtooth(2*pi*60*t,D); % function to draw tringular with duty cycle
+plot(t,y,'g');   % plotiing function
+xlabel('Time');  % show x-axes title
 ylabel('Amplitude'); %show y-exes title
 title('Tringular Wave');
